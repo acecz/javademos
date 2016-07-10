9ae6d3a2ad6d2840f791944732bd9d55ec332f4e
alwyntso <alwyntso@live.com>
2016-07-10 22:07:56 +0800

    init web clawer

---
+++ webcrawler/src/main/java/cz/test/jsoup/web/clawer/TestMain2.java
@@ -0,0 +1,103 @@
+package cz.test.jsoup.web.clawer;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.swing.text.html.HTML.Tag;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class TestMain2 {
+	public static void main(String[] args) throws Exception {
+		for (int i = 0; i < 8; i++) {
+			final int from = i * 10000;
+			new Thread(() -> {
+				try {
+					getWowSpells(from, from + 10000);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}).start();
+		}
+
+	}
+
+	private static void getWowSpells(int from, int to) throws Exception {
+		PrintWriter pw = null;
+		try {
+			pw = new PrintWriter("wowSpells-" + from + "-" + to + ".html");
+			Document doc = new Document("");
+			Element html = doc.appendElement(Tag.HTML.toString());
+			html.appendElement(Tag.HEAD.toString()).appendElement(Tag.META.toString())
+					.attr("http-equiv", "Content-Type").attr("content", "text/html; charset=utf8");
+			Element body = html.appendElement(Tag.BODY.toString());
+			Element table = body.appendElement(Tag.TABLE.toString());
+			table.attr("border", "1");
+			Element thead = table.appendElement(Tag.TR.toString());
+			thead.appendElement(Tag.TH.toString()).appendText("SpellId");
+			thead.appendElement(Tag.TH.toString()).appendText("SpellIcon");
+			thead.appendElement(Tag.TH.toString()).appendText("SpellName");
+			thead.appendElement(Tag.TH.toString()).appendText("SpellDesc");
+			appendContent(table, from, to);
+			pw.write(doc.html());
+			pw.flush();
+		} finally {
+			if (pw != null) {
+				pw.close();
+			}
+		}
+	}
+
+	private static void appendContent(Element table, int from, int to) {
+		String utlFmt = "http://db.duowan.com/wow/spell-%d.html";
+		long start = System.currentTimeMillis();
+		for (int i = from; i < to; i++) {
+			if (i % 1000 == 0) {
+				System.out.println("already spell:" + i + " ,spend time=" + (System.currentTimeMillis() - start));
+			}
+			String url = String.format(utlFmt, i);
+			Document docSpl = null;
+			try {
+				docSpl = loadUrl(url);
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+				try {
+					docSpl = loadUrl(url);
+				} catch (Exception e1) {
+					System.out.println(e.getMessage());
+					try {
+						docSpl = loadUrl(url);
+					} catch (Exception e2) {
+						System.out.println("sepell exception:" + i);
+						e.printStackTrace();
+						continue;
+					}
+				}
+			}
+			Elements contents = docSpl.getElementsByClass("skill-intro-content");
+			if (contents.size() < 1) {
+				System.out.println("sepell empty:" + i);
+				continue;
+			}
+			Element content = contents.get(0);
+			String iconSrc = docSpl.getElementsByClass("pic").get(0).getElementsByTag(Tag.IMG.toString()).get(0)
+					.attr("src");
+			String iconUrl = "http://db.duowan.com/wow/" + iconSrc.replace("\\", "/");
+			String[] nameId = content.getElementsByTag(Tag.H4.toString()).text().split("ID:");
+			String desc = content.getElementsByClass("skill-desc").text();
+			Element tr = table.appendElement(Tag.TR.toString());
+			tr.appendElement(Tag.TD.toString()).appendText(nameId[1]);
+			tr.appendElement(Tag.TD.toString()).appendElement(Tag.IMG.toString()).attr("src", iconUrl);
+			tr.appendElement(Tag.TD.toString()).appendText(nameId[0]);
+			tr.appendElement(Tag.TD.toString()).appendText(desc);
+		}
+	}
+
+	private static Document loadUrl(String url) throws IOException {
+		Document doc = Jsoup.connect(url).execute().parse();
+		return doc;
+	}
+}
